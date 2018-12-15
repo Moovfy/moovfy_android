0e14628b1528b89cc88446a33ae26fcad2e13315
mohachait@gmail.com <Natsu25!>
2018-12-15 11:49:43 +0100

    Solucionat retorn del chat

--- app/src/main/java/com/moovfy/moovfy/ChatActivity.java
+++ app/src/main/java/com/moovfy/moovfy/ChatActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -238,6 +239,16 @@ public class ChatActivity extends AppCompatActivity {
 
     private void setScrollbar(){
         mMessageRecycler.scrollToPosition(mMessageAdapter.getItemCount()-1);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) // Press Back Icon
+        {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
